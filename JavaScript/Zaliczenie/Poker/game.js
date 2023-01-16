396d1fab9004a5bb7b7dfd3d2f438fdc23ae1f25
Anna Plewa <a_no@o2.pl>
2023-01-16 19:54:55 +0100

    poker game solution

--- JavaScript/Zaliczenie/Poker/game.js
+++ JavaScript/Zaliczenie/Poker/game.js
@@ -2,16 +2,31 @@ const cardDeck = require("./cards.js");
 
 console.log(cardDeck);
 
+// [
+//   "Q diamonds(♦)",
+//   "10 diamonds(♦)",
+//   "J diamonds(♦)",
+//   "K diamonds(♦)",
+//   "A diamonds(♦)",
+// ]
+
 // flatten to get rank and suite
 let cardsFlat = cardDeck
   .map((card) => {
     return card.split(" ");
   })
-  .flat(2);
+  .reduce((acc, curr) => acc.concat(curr), []);
+
+console.log(cardsFlat);
 
 // get all ranks in an array
 let cardsInDeckRanks = cardsFlat.filter((value, index, arr) => {
   return index % 2 == 0;
+});
+
+// get all suites in an array
+let cardsInDeckSuite = cardsFlat.filter((value, index, arr) => {
+  return cardsInDeckRanks.indexOf(value) == -1;
 });
 
 for (var i = 0; i < cardsInDeckRanks.length; i++) {
@@ -20,56 +35,34 @@ for (var i = 0; i < cardsInDeckRanks.length; i++) {
   cardsInDeckRanks[i] = cardsInDeckRanks[i].replace(/K/g, "13");
   cardsInDeckRanks[i] = cardsInDeckRanks[i].replace(/A/g, "14");
 }
-
-// get all suites in an array
-let cardsInDeckSuite = cardsFlat.filter((value, index, arr) => {
-  return cardsInDeckRanks.indexOf(value) == -1;
-});
+console.log("cardsInDeckSuite: " + cardsInDeckSuite);
+console.log("cardsInDeckRanks: " + cardsInDeckRanks);
 
 // check if cards are in sequence +1
-const sequenceIsCorrect = () => {
-  cardsInDeckRanks.sort((a, b) => a - b).map(Number);
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] - arr[i - 1] == 1) {
-      return true;
-    }
-  }
-  return false;
-};
+const sortedArrayOfNumbers = cardsInDeckRanks.sort((a, b) => a - b).map(Number);
+console.log("sortedArrayOfNumbers: " + sortedArrayOfNumbers);
 
-const countPairs = (arr) => {
-  let count = 1;
-  let dupChars = [];
-  // making a shallow copy so that the original array remains unaltered
-  const copy = arr.slice();
-  copy.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length; i++) {
-    if (copy[i] === copy[i + 1]) {
-      count++;
-    }
-    for (let i = 0; i < copy.length; i++) {
-      for (let j = i + 1; j < copy.length; j++) {
-        if (copy[i] == copy[j]) {
-          dupChars.push(copy[i]);
-        }
-        // dupChars = copy.filter((element, index) => {
-        //   return copy.indexOf(element) === index;
-        // });
-      }
-    }
-    console.log(dupChars);
-    return count;
-  }
-};
+const sequenceIsCorrect = sortedArrayOfNumbers
+  .map((num, i) => sortedArrayOfNumbers[i + 1] - num)
+  .every((value) => value == 1);
+console.log(sequenceIsCorrect);
 
-let duplicatedCards = cardsInDeckRanks
+const elementCounts = {};
+
+let duplicatedCards = cardsInDeckRanks.forEach((element) => {
+  elementCounts[element] = (elementCounts[element] || 0) + 1;
+});
+
+console.log(elementCounts);
+
+// console.log(duplicatedCards.filter));
+let checkNumberOfDuplicates = Object.values(elementCounts);
+
+console.log(checkNumberOfDuplicates.includes(2));
+
+let duplicatedCardsInArray = checkNumberOfDuplicates
   .sort((a, b) => a - b)
   .filter((card, i, arr) => card === arr[2]);
-
-let duplicatedCardsBackwards = cardsInDeckRanks
-  .sort((a, b) => a - b)
-  .filter((card, i, arr) => card === arr[1]);
-console.log(duplicatedCardsBackwards);
 
 function checkSeniorityOfCards() {
   // Same suite situations
@@ -93,18 +86,25 @@ function checkSeniorityOfCards() {
   } else if (sequenceIsCorrect) {
     // Straight
     console.log("You got straight");
-  } else if (countPairs > 1) {
-    if (duplicatedCards.length === 4) {
-      console.log("You got Four of a king (Quads)");
-    } else if (duplicatedCards.length === 3) {
-      if (duplicatedCardsBackwards.length === 2) {
-        console.log("You got Full house");
-      } else console.log("You got Three of a kind");
-    } else if (duplicatedCards.length === 2 && duplicatedCardsBackwards === 2) {
+  } else if (checkNumberOfDuplicates.includes(4)) {
+    console.log("You got Four of a king (Quads)");
+  } else if (
+    checkNumberOfDuplicates.includes(3) &&
+    checkNumberOfDuplicates.includes(2)
+  ) {
+    console.log("You got Full house");
+  } else if (
+    checkNumberOfDuplicates.includes(3) &&
+    !checkNumberOfDuplicates.includes(2)
+  ) {
+    console.log("You got Three of a kind");
+  } else if (
+    checkNumberOfDuplicates.includes(2) &&
+    !checkNumberOfDuplicates.includes(3)
+  ) {
+    if (duplicatedCardsInArray.length === 2) {
       console.log("You got Two pairs");
-    } else if (duplicatedCards.length === 2 && duplicatedCardsBackwards !== 2) {
-      console.log("You got One pair");
-    }
+    } else console.log("You got One pair");
   } else if (
     cardsInDeckRanks.includes("14") ||
     cardsInDeckRanks.includes("13") ||
