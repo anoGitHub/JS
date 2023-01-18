c41d49a6753004bbb0368724d742fee4abe00054
Anna Plewa <a_no@o2.pl>
2023-01-18 17:24:36 +0100

    solver fixed

--- JavaScript/Zaliczenie/Sudoku/solver.js
+++ JavaScript/Zaliczenie/Sudoku/solver.js
@@ -10,122 +10,58 @@ let sudoku = [
   [5, 0, 8, 0, 0, 2, 1, 0, 3],
 ];
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbersAlreadyUsed = [];
 
-function solver() {
-  let sudokuCopy = [...sudoku];
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (sudoku[i][j] === 0) {
-        let rowAndColumn = [
-          checkRowForDuplicates(i),
-          checkColumnForDuplicates(j),
-          checkSquareForDuplicates(i, j),
-        ].reduce((acc, curr) => acc.concat(curr), []);
+function collectUsedNumbers(board, row, col) {
+  numbersAlreadyUsed = [];
+  for (let k = 0; k < 9; k++) {
+    if (board[row][k] > 0) {
+      numbersAlreadyUsed.push(board[row][k]);
+    }
+    if (board[k][col] > 0) {
+      numbersAlreadyUsed.push(board[k][col]);
+    }
+  }
+  const rowSquare = Math.floor(row / 3) * 3;
+  const colSquare = Math.floor(col / 3) * 3;
+  for (let ro = 0; ro < 3; ro++) {
+    for (let co = 0; co < 3; co++) {
+      if (board[rowSquare + ro][colSquare + co] > 0) {
+        numbersAlreadyUsed.push(board[rowSquare + ro][colSquare + co]);
+      }
+    }
+  }
+  return numbersAlreadyUsed;
+}
 
-        let unique = rowAndColumn
-          .filter((element, index) => {
-            return rowAndColumn.indexOf(element) === index;
-          })
-          .sort();
-        // console.log(unique);
-        const options = [];
+function solver(board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      numbersAlreadyUsed = [];
+
+      if (board[i][j] === 0) {
+        collectUsedNumbers(board, i, j);
+
+        let unique = numbersAlreadyUsed.filter(
+          (element, index, arr) => arr.indexOf(element) === index
+        );
+
+        let options = [];
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
         numbers.some((element) => {
           if (unique.indexOf(element) == -1) {
             options.push(element);
           }
         });
-        console.log(options);
-        if (options.length === 1) {
-          sudokuCopy[i][j] === options[0];
+
+        if (options.length == 1) {
+          board[i][j] = options[0];
+          solver(board);
         }
       }
     }
   }
-  return sudokuCopy;
+  return board;
 }
-
-function checkRowForDuplicates(rowNumber) {
-  // collects all used numbers in a row
-  let tableWithUsedNumbersInRow = [];
-  for (let j = rowNumber; j < 9; j++) {
-    if (sudoku[rowNumber][j] != 0) {
-      tableWithUsedNumbersInRow.push(sudoku[rowNumber][j]);
-    }
-  }
-  return tableWithUsedNumbersInRow;
-}
-
-function checkColumnForDuplicates(columnNumber) {
-  // collects all used numbers in a column
-  let tableWithUsedNumbersInColumn = [];
-  for (let i = columnNumber; i < 9; i++) {
-    if (sudoku[i][columnNumber] != 0) {
-      tableWithUsedNumbersInColumn.push(sudoku[i][columnNumber]);
-    }
-  }
-  return tableWithUsedNumbersInColumn;
-}
-
-function checkSquareForDuplicates(row, col) {
-  // 0,0; 0,3; 0,6; 3,0; 3,3; 3,6; 6,0; 6,3; 6,6.
-  let tableWithUsedNumbersInSquare = [];
-  switch (row) {
-    case 0:
-    case 1:
-    case 2:
-      row = 0;
-      break;
-    case 3:
-    case 4:
-    case 5:
-      row = 1;
-      break;
-    case 6:
-    case 7:
-    case 8:
-      row = 2;
-      break;
-    default:
-      console.log("wrong number row " + row);
-  }
-  switch (col) {
-    case 0:
-    case 1:
-    case 2:
-      col = 0;
-      break;
-    case 3:
-    case 4:
-    case 5:
-      col = 1;
-      break;
-    case 6:
-    case 7:
-    case 8:
-      col = 2;
-      break;
-    default:
-      console.log("wrong number col" + col);
-  }
-  const rowSquare = row * 3;
-  const colSquare = col * 3;
-  for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-      if (sudoku[rowSquare + row][colSquare + col] > 0) {
-        tableWithUsedNumbersInSquare.push(
-          sudoku[rowSquare + row][colSquare + col]
-        );
-      }
-    }
-  }
-  return tableWithUsedNumbersInSquare;
-}
-
-// checkRowForDuplicates(0);
-// checkColumnForDuplicates(0);
-// checkSquareForDuplicates(2, 2);
-// solver();
-// console.log(tableWithUsedNumbersInColumn);
-// console.log(tableWithUsedNumbersInSquare);
-console.log(solver());
+console.log(solver(sudoku));
