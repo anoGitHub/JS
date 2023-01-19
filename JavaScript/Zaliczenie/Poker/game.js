37184f2f980b0941fd413d3d7d82209e049ba35a
Anna Plewa <a_no@o2.pl>
2023-01-19 22:33:05 +0100

    remove conole logs

--- JavaScript/Zaliczenie/Poker/game.js
+++ JavaScript/Zaliczenie/Poker/game.js
@@ -2,22 +2,12 @@ const cardDeck = require("./cards.js");
 
 console.log(cardDeck);
 
-// [
-//   "Q diamonds(♦)",
-//   "10 diamonds(♦)",
-//   "J diamonds(♦)",
-//   "K diamonds(♦)",
-//   "A diamonds(♦)",
-// ]
-
 // flatten to get rank and suite
 let cardsFlat = cardDeck
   .map((card) => {
     return card.split(" ");
   })
   .reduce((acc, curr) => acc.concat(curr), []);
-
-console.log(cardsFlat);
 
 // get all ranks in an array
 let cardsInDeckRanks = cardsFlat.filter((value, index, arr) => {
@@ -35,30 +25,23 @@ for (var i = 0; i < cardsInDeckRanks.length; i++) {
   cardsInDeckRanks[i] = cardsInDeckRanks[i].replace(/K/g, "13");
   cardsInDeckRanks[i] = cardsInDeckRanks[i].replace(/A/g, "14");
 }
-console.log("cardsInDeckSuite: " + cardsInDeckSuite);
-console.log("cardsInDeckRanks: " + cardsInDeckRanks);
 
 // check if cards are in sequence +1
 const sortedArrayOfNumbers = cardsInDeckRanks.sort((a, b) => a - b).map(Number);
-console.log("sortedArrayOfNumbers: " + sortedArrayOfNumbers);
 
 const sequenceIsCorrect = sortedArrayOfNumbers
   .map((num, i) => sortedArrayOfNumbers[i + 1] - num)
   .every((value) => value == 1);
-console.log(sequenceIsCorrect);
 
 const elementCounts = {};
 
-let duplicatedCards = cardsInDeckRanks.forEach((element) => {
+cardsInDeckRanks.forEach((element) => {
   elementCounts[element] = (elementCounts[element] || 0) + 1;
 });
 
 console.log(elementCounts);
-
 // console.log(duplicatedCards.filter));
 let checkNumberOfDuplicates = Object.values(elementCounts);
-
-console.log(checkNumberOfDuplicates.includes(2));
 
 let duplicatedCardsInArray = checkNumberOfDuplicates
   .sort((a, b) => a - b)
